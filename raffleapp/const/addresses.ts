215b43ec6b993d6dabf1ff622eef2f7e3d7d2138
johnie261 <njorogejohn261@gmail.com>
2023-08-10 00:06:12 +0300

    Add navbar

---
+++ raffleapp/const/addresses.ts
@@ -0,0 +1,3 @@
+export const RAFFLE_CONTRACT_ADDRESS='0x0bCe9ca69880AA2794AedAE2655F92A01fFF7DB8'
+
+export const HERO_IMAGE_URL = 'ipfs://QmS1gz2fNL6DfnfebNsY3KkdYh3vT4jcEArkndao6eCk5Z/Untitled%20design%20(14).png';
--- raffleapp/pages/_app.tsx
+++ raffleapp/pages/_app.tsx
@@ -1,16 +1,20 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/navbar";
 
 const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      // clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
     >
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ThirdwebProvider>
   );
 }
